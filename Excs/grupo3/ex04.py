142563716186dcbe6d1f72e84f8d6b9393515c7d
Francisco Mota <franciscoo.mota28@gmail.com>
2023-04-17 11:22:41 +0100

    fiz o exercício 4 do grupo 3

---
+++ Excs/grupo3/ex04.py
@@ -0,0 +1,17 @@
+"""Faça um programa python que leia a partir do utilizador 30 números inteiros positivos e 
+que apresente o menor deles."""
+maior = 0
+menor = 999999
+for i in range(0,30):
+    nr = int(input("Introduza um número inteiro: "))
+    if(nr<menor):
+        menor = nr
+    elif(nr>maior):
+        maior = nr
+
+print(f"O maior número é {maior} e o menor é {menor}")
+
+
+
+
+
