e0a5024e605d1f21e5926b5461c4081f2635d657
Francisco Mota <franciscoo.mota28@gmail.com>
2023-05-13 13:06:43 +0100

    Fiz exercício 7
    Fiz exercício 7  do grupo 3

---
+++ Excs/grupo3/ex07.py
@@ -0,0 +1,13 @@
+"""Elabore um programa python que permita efetuar a soma dos 180 primeiros números 
+inteiros positivos."""
+
+soma = 0 
+
+for i in range(180):
+    soma = soma + i
+
+print(soma)
+
+
+
+
